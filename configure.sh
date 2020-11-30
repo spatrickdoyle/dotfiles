9f0f2377630c73f2c20453ef88b29512abd63dea
Sean Doyle <sean@pop-os.localdomain>
2020-11-30 18:39:34 -0500

    Initial commit

---
+++ configure.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+
+# Move i3 config file to i3 directory
+
+
+
+# Drop in i3 for the window manager
+
+gsettings set org.mate.session.required-components windowmanager 'i3'
+gsettings set org.mate.session required-components-list "['windowmanager', 'panel', 'dock']"
+
+# Move theme and icons to appropriate directories
+
+mkdir -p ~./icons/
+cp ./yaru-blue/Icons/* ~./icons/.
+mkdir -p ~./themes/
+cp ./yaru-blue/Theme/* ~./themes/.
+
+# Set the theme and icons
+
+gsettings set org.mate.interface gtk-theme 'Yaru-Blue-dark'
+gsettings set org.mate.interface icon-theme 'ePapirus'
+
+# Move desktop indicator script to i3 folder
