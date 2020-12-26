0d0efb9668586b67206b01f5e46043738903518b
Sean Doyle <sean@pop-os.localdomain>
2020-12-26 11:39:14 -0500

    Redid directory structure again because pulling themes is easier than packaging them

--- configure.sh
+++ configure.sh
@@ -1,27 +1,41 @@
 #!/usr/bin/env bash
 
 
+# Install i3wm
+
 sudo apt install i3
 
 # Move i3 config file to i3 directory
 
-
+mkdir -p ~/.config/i3/
+cp ./config ~/.config/i3/.
 
 # Drop in i3 for the window manager
 
 gsettings set org.mate.session.required-components windowmanager 'i3'
 gsettings set org.mate.session required-components-list "['windowmanager', 'panel', 'dock']"
 
+# Install theme, icons and fonts
+
+snap install yaru-colors
+
+git clone https://github.com/PapirusDevelopmentTeam/papirus-icon-theme.git ./icons/.
+bash ./icons/install.sh
+
 # Move theme and icons to appropriate directories
 
-mkdir -p ~/.icons/
-cp -r ./yaru-blue/Icons/* ~/.icons/.
-mkdir -p ~/.themes/
-cp -r ./yaru-blue/Theme/* ~/.themes/.
+# mkdir -p ~/.icons/
+# cp -r ./themes/Icons/* ~/.icons/.
+# mkdir -p ~/.themes/
+# cp -r ./themes/Theme/* ~/.themes/.
 
 # Set the theme and icons
 
 gsettings set org.mate.interface gtk-theme 'Yaru-Blue-dark'
 gsettings set org.mate.interface icon-theme 'ePapirus'
+
+# Disable annoying menu hotkey
+
+gsettings set com.solus-project.brisk-menu hot-key ''
 
 # Move desktop indicator script to i3 folder
--- desktops.sh
+++ desktops.sh
@@ -23,34 +23,15 @@ for i in ${DESKTOPS[*]}; do
 	((INDEX=${i:0:1}-1))
 
 	if [ $MONITOR = "HDMI-1" ]; then
-		DESKTOP_IND[$INDEX]="\uf146"
+		DESKTOP_IND[$INDEX]="\u278b"
+	elif [ $MONITOR = "HDMI-2" ]; then
+		DESKTOP_IND[$INDEX]="\u278c"
 	elif [ $MONITOR = "eDP-1" ]; then
-		DESKTOP_IND[$INDEX]="\uf067"
+		DESKTOP_IND[$INDEX]="\u278A"
 	fi
 	if [ $FOCUSED == "true" ]; then
 		DESKTOP_IND[$INDEX]="\uf0c8"
 	fi
 done
 
-BATTERY=$(acpi|sed 's/Battery [0-9]: //; s/%.*//; s/ //;')
-BATTERY_IND=$(echo $BATTERY|sed 's/,.*//')
-if [ $BATTERY_IND = "Discharging" ]; then
-	color="ffffff"
-fi
-if [ $BATTERY_IND = "Unknown" ]; then
-	color="ffffff"
-fi
-if [ $BATTERY_IND = "Charging" ]; then
-	color="d9af4a"
-fi
-if [ $BATTERY_IND = "Full" ]; then
-	color="ffffff"
-	POWER=10
-else
-	POWER=$(echo $BATTERY|sed 's/.*,//')
-	((POWER=$POWER/10))
-fi
-
-#DESKTOP_IND[0]="%{F#"$color"}"${DESKTOP_IND[0]}
-#DESKTOP_IND[POWER]="%{F#777777}"${DESKTOP_IND[POWER]}
 echo -e ${DESKTOP_IND[0]}${DESKTOP_IND[1]}${DESKTOP_IND[2]}${DESKTOP_IND[3]}${DESKTOP_IND[4]}${DESKTOP_IND[5]}${DESKTOP_IND[6]}${DESKTOP_IND[7]}${DESKTOP_IND[8]}${DESKTOP_IND[9]}
